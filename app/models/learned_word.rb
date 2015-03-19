ddaed60d4ed42a3c7a081aafaf307b44e1ca0530
Nguyen Sy Hieu <hieuns.a2@gmail.com>
2015-03-19 10:39:32 +0700

    Fix word filter: correcting user's learned and not learned words result

--- app/models/learned_word.rb
+++ app/models/learned_word.rb
@@ -5,13 +5,13 @@ class LearnedWord < ActiveRecord::Base
 
   scope :in_lesson, ->(lesson_id) {where lesson_id: lesson_id}
   scope :submitted_in_lessons, ->(lessons_ids) do
-    where("lesson_id in (#{lessons_ids}) and choice_id is not NULL")
+    where(lesson_id: lessons_ids).where.not(choice_id: nil)
   end
   scope :submitted_in_lesson, ->(lesson_id) do
-    where("lesson_id = #{lesson_id} and choice_id is not NULL")
+    where(lesson_id: lesson_id).where.not(choice_id: nil)
   end
   scope :remain_in_lessons, ->(lessons_ids) do
-    where("lesson_id in (#{lessons_ids}) and choice_id is NULL")
+    where(lesson_id: lessons_ids, choice_id: nil) 
   end
   scope :remain_in_lesson, ->(lesson_id) do
     where(lesson_id: lesson_id, choice_id: nil)
